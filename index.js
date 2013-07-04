e98fa37c339188b5ef9ff6b4f570483eb1c7d4d2
Christopher Bebry <invalidsyntax@gmail.com>
2013-07-04 18:07:00 -0400

    First commit, basically works. Gets all data upfront. Not perfect with results (some links are grabbed even though they're not monsters) but it's an excellent place to start.

---
+++ index.js
@@ -0,0 +1,91 @@
+var request = require('request'),
+	cheerio = require('cheerio'),
+	async = require('async'),
+	Hoek = require('hoek'),
+	Hapi = require('hapi');
+	
+var server = Hapi.createServer('0.0.0.0', 5497);
+
+server.route({
+	method: 'GET',
+	path: '/',
+	handler: function() {
+		this.reply(masterList);
+	}
+});
+
+var wikibaseURL = 'http://en.wikipedia.org';
+var baseURL = wikibaseURL+'/wiki/List_of_legendary_creatures';
+
+var singleAlphabetURLs = [];
+var masterList = [];
+
+request(baseURL, function(err, response, body) {
+	if (!err && response.statusCode === 200) {
+		var $ = cheerio.load(body);
+		
+		var listing = $('.rellink').find('a');
+		for (var i = 0; i < listing.length; i++) {
+			//console.log(listing[i].attribs.href);
+			singleAlphabetURLs.push(wikibaseURL + listing[i].attribs.href);
+		}
+		
+		//for (var i = 0; i < singleAlphabetURLs.length; i++) {
+			//parseSingleAlphabetPage(singleAlphabetURLs[i], function(alphabetPageListing) {
+				//console.log(alphabetPageListing);
+				
+			//	for (var j = 0; j < alphabetPageListing.length ;j++) {
+			//		//console.log(alphabetPageListing[j]);
+			//		masterList.push(alphabetPageListing[j]);
+			//	}
+				
+			//});
+		//}
+		async.each(singleAlphabetURLs, parseSingleAlphabetPage, function(err) {
+			//its all done
+			masterList = Hoek.unique(masterList);
+			
+			//console.log(masterList);
+			//TODO output
+			server.start();
+			console.log('Server up at ' + server.info.uri + ' !');
+		})
+	}
+});
+
+var pushToMaster = function(data, callback) {
+	masterList.push(data);
+	callback();
+}
+
+var getRemainderLinks = function (alphabetPageListing, callback) {
+	async.each(alphabetPageListing, pushToMaster, function(err) {
+		callback();
+	});
+}
+
+var parseSingleAlphabetPage = function(URL, callback) {
+	request(URL, function(err, response, body) {
+		if (!err && response.statusCode === 200) {
+			var $ = cheerio.load(body);
+			var listing = $('#mw-content-text ul li');
+			var allLinks = [];
+			
+			
+			for (var i = 0; i < listing.length; i++) {
+				
+			}
+			async.each(listing, function(input, zcallback) {
+				if (input.children[0].attribs) {
+					allLinks.push(wikibaseURL+input.children[0].attribs.href);
+				}
+				zcallback();
+			}, function(err) {
+				//done
+				getRemainderLinks(allLinks, function() {
+					callback();
+				});
+			});
+		}
+	});
+}
